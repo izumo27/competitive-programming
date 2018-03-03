5d86d29017052b194d0ff2df57e113e2b5e41a50
izumo27 <22386882+izumo27@users.noreply.github.com>
2018-03-03 18:05:42 +0900

    abc70

---
+++ atcoder/abc70/b.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+	int a,b,c,d;
+	scanf("%d%d%d%d", &a, &b, &c, &d);
+
+	if (a > c) c = a;
+	if (b > d) b = d;
+	if (c < b) printf("%d\n", b-c);
+	else printf("0\n");
+	return 0;
+}

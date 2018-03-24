9479ab7b512d34592dc9a5352eac6e3c901a8bbb
izumo27 <22386882+izumo27@users.noreply.github.com>
2018-03-25 02:02:58 +0900

    Add "AtCoder Beginners Selection" 7

---
+++ AtCoder/ABC/061-070/061/b.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long int llint;
+
+int main(){
+	int n, m, i;
+	int a[60], b[60];
+	cin>>n>>m;
+	for(i=1; i<=m; i++) cin>>a[i]>>b[i];
+	int ans[n];
+	for(i=1; i<=n; i++) ans[i]=0;
+	for(i=1; i<=m; i++){
+		ans[a[i]]++;
+		ans[b[i]]++;
+	}
+	for(i=1; i<=n; i++) cout<<ans[i]<<endl;
+	return 0;
+}

5a24bd250948ee4688c0919cef5afde80200ea14
izumo27 <22386882+izumo27@users.noreply.github.com>
2018-04-07 23:01:20 +0900

    Add ABC093 A,B,c

---
+++ AtCoder/ABC/091-100/093/a.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+int main(){
+	string s;
+	cin>>s;
+	sort(begin(s), end(s));
+	s=="abc" ? cout<<"Yes"<<endl : cout<<"No"<<endl;
+	return 0;
+}

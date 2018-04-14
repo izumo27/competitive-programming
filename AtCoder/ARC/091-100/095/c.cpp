88b0688a90f2ddf6ae38dd2efd01bd0cfd2de474
izumo27 <22386882+izumo27@users.noreply.github.com>
2018-04-14 23:02:16 +0900

    Add ABC094 A,B,C,D

---
+++ AtCoder/ARC/091-100/095/c.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+int main(){
+	int n;
+	int x[210000];
+	cin>>n;
+	for(int i=0; i<n; i++) cin>>x[i];
+	int y[210000];
+	for(int i=0; i<n; i++) y[i]=x[i];
+	sort(y, y+n);
+	for(int i=0; i<n; i++){
+		if(x[i]<=y[(n/2)-1]) cout<<y[n/2]<<endl;
+		else cout<<y[(n/2)-1]<<endl;
+	}
+	return 0;
+}

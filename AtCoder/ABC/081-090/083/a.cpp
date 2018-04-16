a335e508d15f793d71ec8c33640ed555c7648d74
izumo27 <22386882+izumo27@users.noreply.github.com>
2018-04-17 01:42:53 +0900

    Add "Ants Book" 2-1-1

---
+++ AtCoder/ABC/081-090/083/a.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+int main(){
+	int a, b, c, d;
+	cin>>a>>b>>c>>d;
+	if(a+b>c+d) cout<<"Left"<<endl;
+	if(a+b==c+d) cout<<"Balanced"<<endl;
+	if(a+b<c+d) cout<<"Right"<<endl;
+	return 0;
+}

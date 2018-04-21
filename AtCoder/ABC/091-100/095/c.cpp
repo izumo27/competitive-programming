c1edfb33953bcc5c800bb0bf21264066495230a2
izumo27 <22386882+izumo27@users.noreply.github.com>
2018-04-22 01:49:13 +0900

    Add ABC095 A,B,C,D

---
+++ AtCoder/ABC/091-100/095/c.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+
+#define REP(i, n) for(int i=0; i<n; ++i)
+
+int main(){
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	int a, b, c, x, y;
+	cin>>a>>b>>c>>x>>y;
+	int m;
+	x>y ? m=a : m=b;
+	int ans;
+	if(a+b<=c*2) ans=a*x+b*y;
+	else ans=c*min(x, y)*2+min(m, c*2)*abs(x-y);
+	cout<<ans<<'\n';
+	return 0;
+}

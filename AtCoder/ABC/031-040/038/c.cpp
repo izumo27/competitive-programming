fccac8c7264422f1015ad62b814a75c2b7ef8ac7
izumo27 <22386882+izumo27@users.noreply.github.com>
2018-05-31 19:54:30 +0900

    Shakutori Hou

---
+++ AtCoder/ABC/031-040/038/c.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+
+#define REP(i, n) for(int i=0; i<(n); ++i)
+#define FOR(i, a, b) for(int i=(a); i<(b); ++i)
+#define FORR(i, a, b) for(int i=(b)-1; i>=(a); --i)
+
+#define DEBUG(x) cout<<#x<<": "<<x<<'\n'
+#define DEBUG_VEC(v) cout<<#v<<":";REP(i, v.size())cout<<' '<<v[i];cout<<'\n'
+#define ALL(a) (a).begin(), (a).end()
+
+#define CHMIN(a, b) a=min((a), (b))
+#define CHMAX(a, b) a=max((a), (b))
+
+const int MOD=1000000007;
+#define FIX(a) ((a)%MOD+MOD)%MOD
+
+const double EPS=1e-11;
+#define EQ0(x) (abs((x))<EPS)
+#define EQ(a, b) (abs((a)-(b))<EPS)
+
+int main(){
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	int n, a[114514];
+	cin>>n;
+	REP(i, n){
+		cin>>a[i];
+	}
+	ll r=0, ans=0;
+	for(ll l; l<n; ++l){
+		int tmp=0;
+		while(r<n && tmp<a[r]){
+			tmp=a[r++];
+		}
+		ans+=(r-l)*(r-l+1)/2;
+		l=r-1;
+	}
+	cout<<ans<<'\n';
+	return 0;
+}

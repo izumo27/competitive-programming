4eff2c04a94721d949ff515e89a56cf93da40d50
izumo27 <22386882+izumo27@users.noreply.github.com>
2018-06-17 03:08:17 +0900

    Update combination

--- Library/comb.cpp
+++ Library/comb.cpp
@@ -43,7 +43,7 @@ ll fact[514514], fact_inv[514514];
 //     }
 // }
 
-ll mod_pow(ll a, ll b){
+ll power(ll a, ll b){
 	ll res=1;
 	while(b>0){
 		if(b&1){
@@ -68,7 +68,7 @@ int main(){
 	REP(i, n){
 		fact[i+1]=fact[i]*(i+1)%MOD;
 	}
-	fact_inv[n]=mod_pow(fact[n], MOD-2);
+	fact_inv[n]=power(fact[n], MOD-2);
 	FORR(i, 0, n){
 		fact_inv[i]=fact_inv[i+1]*(i+1)%MOD;
 	}

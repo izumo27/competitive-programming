c55aa3b5bbddbd40a493f0c2780605dd8bade39b
izumo27 <22386882+izumo27@users.noreply.github.com>
2018-10-15 00:20:07 +0900

    Add CODE-FESTIVAL 2018 qual-B A,B,C

---
+++ AtCoder/CODE-FESTIVAL/2018/qual-b/b.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+
+#define REP(i, n) for(int i=0; i<(n); ++i)
+#define FOR(i, a, b) for(int i=(a); i<(b); ++i)
+#define FORR(i, a, b) for(int i=(b)-1; i>=(a); --i)
+
+#define DEBUG(x) cout<<#x<<": "<<x<<'\n'
+#define DEBUG_VEC(v) cout<<#v<<":";REP(i, v.size())cout<<' '<<v[i];cout<<'\n'
+#define ALL(a) (a).begin(), (a).end()
+
+#define CHMIN(a, b) a=min((a), (b))
+#define CHMAX(a, b) a=max((a), (b))
+
+const ll MOD=1000000007ll;
+// const ll MOD=998244353ll;
+#define FIX(a) ((a)%MOD+MOD)%MOD
+
+const double EPS=1e-11;
+#define EQ0(x) (abs((x))<EPS)
+#define EQ(a, b) (abs((a)-(b))<EPS)
+
+int main(){
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	int n, x;
+	pii ab[114];
+	cin>>n>>x;
+	REP(i, n){
+		cin>>ab[i].second>>ab[i].first;
+	}
+	sort(ab, ab+n);
+	int ans=0;
+	FORR(i, 0, n){
+		if(i==n-1){
+			ans+=(ab[i].second+x)*ab[i].first;
+		}
+		else{
+			ans+=ab[i].second*ab[i].first;
+		}
+	}
+	cout<<ans<<'\n';
+	return 0;
+}

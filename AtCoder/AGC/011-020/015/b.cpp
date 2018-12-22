9a26bcfbd61786cfc14141ab42faa15f8099b0ba
izumo27 <22386882+izumo27@users.noreply.github.com>
2018-12-23 00:16:27 +0900

    Add AGC015,017 B

---
+++ AtCoder/AGC/011-020/015/b.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+
+#define REP(i, n) for(int i=0; i<(n); ++i)
+#define FOR(i, a, b) for(int i=(a); i<(b); ++i)
+#define FORR(i, a, b) for(int i=(b)-1; i>=(a); --i)
+
+#define DEBUG(x) cout<<#x<<": "<<x<<'\n'
+#define DEBUG_VEC(v) cout<<#v<<":";REP(i, v.size())cout<<' '<<v[i];cout<<'\n'
+#define ALL(a) (a).begin(), (a).end()
+
+#define CHMIN(a, b) a=min((a), (b))
+#define CHMAX(a, b) a=max((a), (b))
+
+const ll MOD=1000000007ll;
+// const ll MOD=998244353ll;
+#define FIX(a) ((a)%MOD+MOD)%MOD
+
+const double EPS=1e-11;
+#define EQ0(x) (abs((x))<EPS)
+#define EQ(a, b) (abs((a)-(b))<EPS)
+
+int main(){
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	string s;
+	cin>>s;
+	int n=s.size();
+	ll ans=2*(n-1);
+	FOR(i, 1, n-1){
+		if(s[i]=='U'){
+			ans+=n-i-1+2*i;
+		}
+		else{
+			ans+=2*(n-i-1)+i;
+		}
+	}
+	cout<<ans<<'\n';
+	return 0;
+}

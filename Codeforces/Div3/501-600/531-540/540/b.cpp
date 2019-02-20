a370053298a940569b3ac77d6416a2b52d04a792
izumo27 <22386882+izumo27@users.noreply.github.com>
2019-02-20 20:37:08 +0900

    Add Codeforces 540 A,B,D1,D2

---
+++ Codeforces/Div3/501-600/531-540/540/b.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+
+#define REP(i, n) for(int i=0; i<(n); ++i)
+#define FOR(i, a, b) for(int i=(a); i<(b); ++i)
+#define FORR(i, a, b) for(int i=(b)-1; i>=(a); --i)
+
+#define DEBUG(x) cout<<#x<<": "<<x<<'\n'
+#define DEBUG_VEC(v) cout<<#v<<":";REP(i, v.size())cout<<' '<<v[i];cout<<'\n'
+#define ALL(a) (a).begin(), (a).end()
+
+#define CHMIN(a, b) a=min((a), (b))
+#define CHMAX(a, b) a=max((a), (b))
+
+const ll MOD=1000000007ll;
+// const ll MOD=998244353ll;
+#define FIX(a) ((a)%MOD+MOD)%MOD
+
+const double EPS=1e-11;
+#define EQ0(x) (abs((x))<EPS)
+#define EQ(a, b) (abs((a)-(b))<EPS)
+
+int sum1[252521], sum2[252521];
+
+int main(){
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	int n, a[252521];
+	cin>>n;
+	REP(i, n){
+		cin>>a[i];
+		sum1[i+1]=sum1[i];
+		sum2[i+1]=sum2[i];
+		if(i&1){
+			sum1[i+1]+=a[i];
+		}
+		else{
+			sum2[i+1]+=a[i];
+		}
+	}
+	int ans=0;
+	REP(i, n){
+		if(sum2[i]+sum1[n]-sum1[i+1]==sum1[i]+sum2[n]-sum2[i+1]){
+			++ans;
+			// DEBUG(i);
+		}
+	}
+	cout<<ans<<'\n';
+	return 0;
+}
---
+++ Codeforces/Div3/501-600/531-540/540/d.cpp
@@ -0,0 +1,82 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+
+#define REP(i, n) for(int i=0; i<(n); ++i)
+#define FOR(i, a, b) for(int i=(a); i<(b); ++i)
+#define FORR(i, a, b) for(int i=(b)-1; i>=(a); --i)
+
+#define DEBUG(x) cout<<#x<<": "<<x<<'\n'
+#define DEBUG_VEC(v) cout<<#v<<":";REP(i, v.size())cout<<' '<<v[i];cout<<'\n'
+#define ALL(a) (a).begin(), (a).end()
+
+#define CHMIN(a, b) a=min((a), (b))
+#define CHMAX(a, b) a=max((a), (b))
+
+const ll MOD=1000000007ll;
+// const ll MOD=998244353ll;
+#define FIX(a) ((a)%MOD+MOD)%MOD
+
+const double EPS=1e-11;
+#define EQ0(x) (abs((x))<EPS)
+#define EQ(a, b) (abs((a)-(b))<EPS)
+
+ll n, m;
+ll a[252521];
+ll sum[252521];
+
+bool is_ok(ll mid){
+	if(n<=mid){
+		return true;
+	}
+	vl v[mid];
+	REP(i, n){
+		v[i%mid].push_back(a[i]);
+	}
+	ll cnt=0;
+	REP(i, mid){
+		REP(j, v[i].size()){
+			cnt+=max(0ll, v[i][j]-j);
+		}
+	}
+	// if(mid==2){
+	// 	DEBUG(cnt);
+	// }
+	return cnt>=m;
+}
+
+int main(){
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	cin>>n>>m;
+	REP(i, n){
+		cin>>a[i];
+	}
+	sort(a, a+n, greater<ll>());
+	REP(i, n){
+		sum[i+1]=sum[i]+a[i];
+	}
+	if(sum[n]<m){
+		cout<<-1<<'\n';
+		return 0;
+	}
+	ll lb=0, ub=1e10;
+	while(ub-lb>1){
+		ll mid=(lb+ub)/2;
+		if(is_ok(mid)){
+			ub=mid;
+		}
+		else{
+			lb=mid;
+		}
+	}
+	cout<<ub<<'\n';
+	return 0;
+}

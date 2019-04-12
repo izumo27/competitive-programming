4a55a5cb6d9f5a5bbd852b8970299388dba78c75
izumo27 <22386882+izumo27@users.noreply.github.com>
2019-04-13 00:57:40 +0900

    Fix typo

--- Library/prime.cpp
+++ Library/prime.cpp
@@ -13,7 +13,8 @@ typedef pair<ll, ll> pll;
 #define FOR(i, a, b) for(int i=(a); i<(b); ++i)
 #define FORR(i, a, b) for(int i=(b)-1; i>=(a); --i)
 
-#define DEBUG(x) cout<<#x<<": "<<x<<'\n'#define DEBUG(x) cout<<#x<<": "<<(x)<<'\n'#define DEBUG_VEC(v) cout<<#v<<":";REP(i, v.size())cout<<' '<<v[i];cout<<'\n'
+#define DEBUG(x) cout<<#x<<": "<<(x)<<'\n'
+#define DEBUG_VEC(v) cout<<#v<<":";REP(i, v.size())cout<<' '<<v[i];cout<<'\n'
 #define ALL(a) (a).begin(), (a).end()
 
 template<typename T> inline void CHMAX(T& a, const T b) {if(a<b) a=b;}

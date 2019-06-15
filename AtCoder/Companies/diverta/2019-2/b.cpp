9bc0206acccc747a1a6dbff4d19184194c760dc4
izumo27 <22386882+izumo27@users.noreply.github.com>
2019-06-16 00:55:16 +0900

    Add diverta2019-2 A,B,C,D

---
+++ AtCoder/Companies/diverta/2019-2/b.cpp
@@ -0,0 +1,144 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+
+#define REP(i, n) for(int i=0; i<(n); ++i)
+#define FOR(i, a, b) for(int i=(a); i<(b); ++i)
+#define FORR(i, a, b) for(int i=(b)-1; i>=(a); --i)
+
+#define DEBUG(x) cout<<#x<<": "<<(x)<<'\n'
+#define DEBUG_VEC(v) cout<<#v<<":";REP(i, v.size())cout<<' '<<v[i];cout<<'\n'
+#define ALL(a) (a).begin(), (a).end()
+
+template<typename T> inline void CHMAX(T& a, const T b) {if(a<b) a=b;}
+template<typename T> inline void CHMIN(T& a, const T b) {if(a>b) a=b;}
+
+constexpr ll MOD=1000000007ll;
+// constexpr ll MOD=998244353ll;
+#define FIX(a) ((a)%MOD+MOD)%MOD
+
+const double EPS=1e-11;
+#define EQ0(x) (abs((x))<EPS)
+#define EQ(a, b) (abs((a)-(b))<EPS)
+
+int gcd(int a, int b){
+	if(b==0){
+		return a;
+	}
+	return gcd(b, a%b);
+}
+
+struct UnionFind{
+	vi par;
+	vi rank;
+
+	UnionFind(int n=1){
+		init(n);
+	}
+
+	// n要素で初期化
+	void init(int n=1){
+		par.resize(n);
+		rank.resize(n);
+		REP(i, n){
+			par[i]=i;
+			rank[i]=0;
+		}
+	}
+
+	// xの根を求める
+	int find(int x){
+		if(par[x]==x){
+			return x;
+		}
+		else{
+			return par[x]=find(par[x]);
+		}
+	}
+
+	// xとyが属する集合を併合
+	bool unite(int x, int y){
+		x=find(x);
+		y=find(y);
+		if(x==y){
+			return false;
+		}
+		if(rank[x]<rank[y]){
+			par[x]=y;
+		}
+		else{
+			par[y]=x;
+			if(rank[x]==rank[y]){
+				++rank[x];
+			}
+		}
+		return true;
+	}
+
+	// xとyが同じ集合に属するか判定
+	bool issame(int x, int y){
+		return find(x)==find(y);
+	}
+};
+
+bool used[60];
+
+int main(){
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	// cout<<setprecision(10)<<fixed;
+	int n;
+	pii xy[60];
+	cin>>n;
+	REP(i, n){
+		cin>>xy[i].first>>xy[i].second;
+		--xy[i].first;
+		--xy[i].second;
+	}
+	if(n==1){
+		cout<<1<<'\n';
+		return 0;
+	}
+	int ans=1e9;
+	REP(i, n){
+		REP(j, n){
+			if(i==j){
+				continue;
+			}
+			int dx=xy[j].first-xy[i].first;
+			int dy=xy[j].second-xy[i].second;
+			if(dx==0 && dy==0){
+				continue;
+			}
+			UnionFind uf(n);
+			REP(a, n){
+				REP(b, n){
+					if((xy[b].first-xy[a].first==dx) && (xy[b].second-xy[a].second==dy)){
+						uf.unite(a, b);
+					}
+				}
+			}
+			REP(a, n){
+				used[a]=false;
+			}
+			int cnt=0;
+			REP(a, n){
+				int tmp=uf.find(a);
+				if(!used[tmp]){
+					++cnt;
+				}
+				used[tmp]=true;
+			}
+			CHMIN(ans, cnt);
+		}
+	}
+	cout<<ans<<'\n';
+	return 0;
+}
---
+++ AtCoder/Companies/diverta/2019-2/c.cpp
@@ -0,0 +1,100 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+
+#define REP(i, n) for(int i=0; i<(n); ++i)
+#define FOR(i, a, b) for(int i=(a); i<(b); ++i)
+#define FORR(i, a, b) for(int i=(b)-1; i>=(a); --i)
+
+#define DEBUG(x) cout<<#x<<": "<<(x)<<'\n'
+#define DEBUG_VEC(v) cout<<#v<<":";REP(i, v.size())cout<<' '<<v[i];cout<<'\n'
+#define ALL(a) (a).begin(), (a).end()
+
+template<typename T> inline void CHMAX(T& a, const T b) {if(a<b) a=b;}
+template<typename T> inline void CHMIN(T& a, const T b) {if(a>b) a=b;}
+
+constexpr ll MOD=1000000007ll;
+// constexpr ll MOD=998244353ll;
+#define FIX(a) ((a)%MOD+MOD)%MOD
+
+const double EPS=1e-11;
+#define EQ0(x) (abs((x))<EPS)
+#define EQ(a, b) (abs((a)-(b))<EPS)
+
+int main(){
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	// cout<<setprecision(10)<<fixed;
+	int n, a[125252];
+	cin>>n;
+	int sum=0;
+	REP(i, n){
+		cin>>a[i];
+		sum+=abs(a[i]);
+	}
+	if(n==2){
+		if((a[0]-a[1])<(a[1]-a[0])){
+			cout<<a[1]-a[0]<<'\n';
+			cout<<a[1]<<' '<<a[0]<<'\n';
+		}
+		else{
+			cout<<a[0]-a[1]<<'\n';
+			cout<<a[0]<<' '<<a[1]<<'\n';
+		}
+		return 0;
+	}
+	sort(a, a+n);
+	if(a[n-2]>=0 && a[n-1]>=0){
+		if(a[0]>=0){
+			cout<<sum-2*a[0]<<'\n';
+		}
+		else{
+			cout<<sum<<'\n';
+		}
+		int tmp=0;
+		cout<<a[0]<<' '<<a[n-1]<<'\n';
+		tmp=a[0]-a[n-1];
+		int idx=0;
+		FORR(i, 1, n-2){
+			if(a[i]<0){
+				idx=i;
+				break;
+			}
+			cout<<tmp<<' '<<a[i]<<'\n';
+			tmp-=a[i];
+		}
+		cout<<a[n-2]<<' '<<tmp<<'\n';
+		tmp=a[n-2]-tmp;
+		FOR(i, 1, idx+1){
+			cout<<tmp<<' '<<a[i]<<'\n';
+			tmp-=a[i];
+		}
+	}
+	else if(a[n-1]>=0){
+		cout<<sum<<'\n';
+		cout<<a[n-1]<<' '<<a[0]<<'\n';
+		int tmp=a[n-1]-a[0];
+		FOR(i, 1, n-1){
+			cout<<tmp<<' '<<a[i]<<'\n';
+			tmp-=a[i];
+		}
+	}
+	else{
+		cout<<sum+2*a[n-1]<<'\n';
+		int tmp=0;
+		cout<<a[n-1]<<' '<<a[n-2]<<'\n';
+		tmp=a[n-1]-a[n-2];
+		FORR(i, 0, n-2){
+			cout<<tmp<<' '<<a[i]<<'\n';
+			tmp-=a[i];
+		}
+	}
+	return 0;
+}

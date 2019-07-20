d83540f12607516bb7105e3a56105d0f7f788ba9
izumo27 <22386882+izumo27@users.noreply.github.com>
2019-07-20 23:31:15 +0900

    Add ABC134 A-E

---
+++ AtCoder/ABC/131-140/134/c.cpp
@@ -0,0 +1,69 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+
+#define REP(i, n) for(int i=0; i<(n); ++i)
+#define FOR(i, a, b) for(int i=(a); i<(b); ++i)
+#define FORR(i, a, b) for(int i=(b)-1; i>=(a); --i)
+
+#define DEBUG(x) cout<<#x<<": "<<(x)<<'\n'
+#define DEBUG_VEC(v) cout<<#v<<":";REP(i, v.size())cout<<' '<<v[i];cout<<'\n'
+#define ALL(a) (a).begin(), (a).end()
+
+template<typename T> inline void CHMAX(T& a, const T b) {if(a<b) a=b;}
+template<typename T> inline void CHMIN(T& a, const T b) {if(a>b) a=b;}
+
+constexpr ll MOD=1000000007ll;
+// constexpr ll MOD=998244353ll;
+#define FIX(a) ((a)%MOD+MOD)%MOD
+
+const double EPS=1e-11;
+#define EQ0(x) (abs((x))<EPS)
+#define EQ(a, b) (abs((a)-(b))<EPS)
+
+int main(){
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	// cout<<setprecision(10)<<fixed;
+	int n, a[252525];
+	cin>>n;
+	int ma=0;
+	REP(i, n){
+		cin>>a[i];
+		CHMAX(ma, a[i]);
+	}
+	int cnt=0;
+	REP(i, n){
+		if(a[i]==ma){
+			++cnt;
+		}
+	}
+	if(cnt>=2){
+		REP(i, n){
+			cout<<ma<<'\n';
+		}
+	}
+	else{
+		int b[252525];
+		REP(i, n){
+			b[i]=a[i];
+		}
+		sort(b, b+n);
+		REP(i, n){
+			if(a[i]==ma){
+				cout<<b[n-2]<<'\n';
+			}
+			else{
+				cout<<ma<<'\n';
+			}
+		}
+	}
+	return 0;
+}

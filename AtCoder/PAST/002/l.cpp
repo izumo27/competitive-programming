ba173ebd2256786bb2c7ed90c516358b540f381c
izumo27 <22386882+izumo27@users.noreply.github.com>
2020-05-17 02:30:14 +0900

    Create Universities folder

---
+++ AtCoder/PAST/002/l.cpp
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+
+#define REP(i, n) for(int i=0; i<(n); ++i)
+#define FOR(i, a, b) for(int i=(a); i<(b); ++i)
+#define FORR(i, a, b) for(int i=(b)-1; i>=(a); --i)
+
+#define DEBUG(x) cout<<#x<<": "<<(x)<<'\n'
+#define DEBUG_VEC(v) cout<<#v<<":";REP(i, v.size())cout<<' '<<v[i];cout<<'\n'
+#define ALL(a) (a).begin(), (a).end()
+
+template<typename T> inline void CHMAX(T& a, const T b) {if(a<b) a=b;}
+template<typename T> inline void CHMIN(T& a, const T b) {if(a>b) a=b;}
+
+constexpr ll MOD=1000000007ll;
+// constexpr ll MOD=998244353ll;
+#define FIX(a) ((a)%MOD+MOD)%MOD
+
+const double EPS=1e-9;
+#define EQ0(x) (abs((x))<EPS)
+#define EQ(a, b) (abs((a)-(b))<EPS)
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+  // cout<<setprecision(10)<<fixed;
+  int n, k, d, a[252521];
+  cin>>n>>k>>d;
+  REP(i, n){
+    cin>>a[i];
+  }
+  vi ans;
+  int idx=0, cur=-d;
+  priority_queue<pii, vector<pii>, greater<pii>> pq;
+  REP(i, k){
+    while(idx<n-(k-1-i)*d){
+      pq.emplace(a[idx], idx);
+      ++idx;
+    }
+    while(true){
+      if(pq.empty()){
+        cout<<-1<<'\n';
+        return 0;
+      }
+      auto [x, y]=pq.top();pq.pop();
+      if(y-cur>=d){
+        ans.emplace_back(x);
+        cur=y;
+        break;
+      }
+    }
+  }
+  REP(i, ans.size()){
+    cout<<ans[i]<<" \n"[i==ans.size()];
+  }
+  return 0;
+}

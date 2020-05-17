f49c3c936f24e6472b80730fb65417f216c24223
izumo27 <22386882+izumo27@users.noreply.github.com>
2020-05-18 01:54:46 +0900

    Add ABC168 A-E

---
+++ AtCoder/ABC/161-170/168/c.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+
+#define REP(i, n) for(int i=0; i<(n); ++i)
+#define FOR(i, a, b) for(int i=(a); i<(b); ++i)
+#define FORR(i, a, b) for(int i=(b)-1; i>=(a); --i)
+
+#define DEBUG(x) cout<<#x<<": "<<(x)<<'\n'
+#define DEBUG_VEC(v) cout<<#v<<":";REP(i, v.size())cout<<' '<<v[i];cout<<'\n'
+#define ALL(a) (a).begin(), (a).end()
+
+template<typename T> inline void CHMAX(T& a, const T b) {if(a<b) a=b;}
+template<typename T> inline void CHMIN(T& a, const T b) {if(a>b) a=b;}
+
+constexpr ll MOD=1000000007ll;
+// constexpr ll MOD=998244353ll;
+#define FIX(a) ((a)%MOD+MOD)%MOD
+
+const double EPS=1e-9;
+#define EQ0(x) (abs((x))<EPS)
+#define EQ(a, b) (abs((a)-(b))<EPS)
+
+constexpr long double pi = 3.141592653589793;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+  cout<<setprecision(12)<<fixed;
+  long double a, b, h, m;
+  cin>>a>>b>>h>>m;
+  long double A=(h+m/60)/12*2*pi, B=m/60*2*pi;
+  long double C=abs(A-B);
+  cout<<sqrt(a*a+b*b-2*a*b*cos(C))<<'\n';
+  return 0;
+}

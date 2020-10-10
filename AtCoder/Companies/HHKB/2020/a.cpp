544fa8fd1ec84a7d6977f16434c298d81ca3d6c0
izumo27 <22386882+izumo27@users.noreply.github.com>
2020-10-11 00:07:51 +0900

    Add HHKB2020 A-E

---
+++ AtCoder/Companies/HHKB/2020/a.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+
+#define REP(i, n) for(int i=0; i<(n); ++i)
+#define FOR(i, a, b) for(int i=(a); i<(b); ++i)
+#define FORR(i, a, b) for(int i=(b)-1; i>=(a); --i)
+
+#define DEBUG(x) cout<<#x<<": "<<(x)<<'\n'
+#define DEBUG_VEC(v) cout<<#v<<":";REP(i, v.size())cout<<' '<<v[i];cout<<'\n'
+#define ALL(a) (a).begin(), (a).end()
+
+template<typename T> inline void CHMAX(T& a, const T b) {if(a<b) a=b;}
+template<typename T> inline void CHMIN(T& a, const T b) {if(a>b) a=b;}
+
+constexpr ll MOD=1000000007ll;
+// constexpr ll MOD=998244353ll;
+#define FIX(a) ((a)%MOD+MOD)%MOD
+
+const double EPS=1e-9;
+#define EQ0(x) (abs((x))<EPS)
+#define EQ(a, b) (abs((a)-(b))<EPS)
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+  // cout<<setprecision(10)<<fixed;
+  char s, t;
+  cin>>s>>t;
+  if(s=='Y'){
+    if(t=='a'){
+      cout<<'A'<<'\n';
+    }
+    if(t=='b'){
+      cout<<'B'<<'\n';
+    }
+    if(t=='c'){
+      cout<<'C'<<'\n';
+    }
+  }
+  else{
+    cout<<t<<'\n';
+  }
+  return 0;
+}
